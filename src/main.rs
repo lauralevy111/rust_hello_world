f7130a921fd793427ab1c6ee229985aecdff4999
Laura Levy <llevy510@gmail.com>
2020-08-28 14:46:44 -0400

    created a tuple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
 
     let logical bool = true;
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+   let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                     -1i8, -2i16, -3i32, -4i64,
+                     0.1f32, 0.2f64,
+                     'a', true);
+
 }
