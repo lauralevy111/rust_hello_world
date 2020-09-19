ea5f8defb9cb0bd5fc697bef96324e52100a2bb0
Laura Levy <llevy510@gmail.com>
2020-09-19 11:07:03 -0400

    added function inspect

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,26 @@ struct Rectangle {
 }
 */
 
+// A function which takes a `WebEvent` enum as an argument and
+// returns nothing.
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // Destructure `c` from inside the `enum`.
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // Destructure `Click` into `x` and `y`.
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        },
+    }
+}
 
 fn main() {
+
+
+
 /*
     let logical bool = true;
     //println!("Hello, world!");
