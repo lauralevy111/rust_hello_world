520e5fe7f134542885d56904f9e840df388dc3aa
Laura Levy <llevy510@gmail.com>
2020-09-19 11:08:31 -0400

    added code that implements inspect fnand WebEvent enum

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,18 @@ fn inspect(event: WebEvent) {
 
 fn main() {
 
+        let pressed = WebEvent::KeyPress('x');
+        // `to_owned()` creates an owned `String` from a string slice.
+        let pasted  = WebEvent::Paste("my text".to_owned());
+        let click   = WebEvent::Click { x: 20, y: 80 };
+        let load    = WebEvent::PageLoad;
+        let unload  = WebEvent::PageUnload;
 
+        inspect(pressed);
+        inspect(pasted);
+        inspect(click);
+        inspect(load);
+        inspect(unload);
 
 /*
     let logical bool = true;
