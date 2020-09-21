8234fafeb834b8ba77309231e5831e5e60cc9913
Laura Levy <llevy510@gmail.com>
2020-09-21 10:43:52 -0400

    removed all practice code

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,7 @@
 #[derive(Debug)]
 
-//use std::mem;
-
-// This function borrows a slice
-//fn analyze_slice(slice: &[i32]) {
-//    println!("first element of the slice: {}", slice[0]);
-//    println!("the slice has {} elements", slice.len());
-//}
-
-
-/*
-struct Person<'a> {
-    // The 'a defines a lifetime
-    name: &'a str,
-    age: u8,
-}
-
-// A unit struct
-struct Unit;
-
-// A tuple struct
-struct Pair(i32, f32);
-
-
-// A struct with two fields
-struct Point {
-    x: f32,
-    y: f32,
-}
-#[allow(dead_code)]
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point,
-    bottom_right: Point,
-}
-
-fn calculate_area(rectangle: Rectangle)-> f32{
-        let y = rectangle.top_left.y - rectangle.bottom_right.y;
-        let x = rectangle.bottom_right.x - rectangle.top_left.x;
-
-        let area = x*y;
-        return area;
-
-}
-*/
-
 
 fn main() {
 
- 
+
 }
