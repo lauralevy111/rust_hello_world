3b2bcc5db18b5e884dd29e2d670c24a967da3e29
Laura Levy <llevy510@gmail.com>
2020-09-22 13:52:11 -0400

    removed enums from previous practice

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
 #![allow(dead_code)]
 
-enum Status {
-    Rich,
-    Poor,
-
-}
-
-enum Work {
-    Civilian,
-    Soldier,
-}
 
 fn main() {
     // Explicitly `use` each name so they are available without
