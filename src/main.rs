fe8baa20be551609ae376536d3ceb3d547ef3f38
Laura Levy <llevy510@gmail.com>
2020-09-22 13:54:39 -0400

    added cons & nil to enum List

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use crate::List::*;
 
 enum List{
 
+        // Cons: Tuple struct that wraps an element and a pointer to the next node
+        Cons(u32, Box<List>),
+        // Nil: A node that signifies the end of the linked list
+        Nil,
 
 }
 
